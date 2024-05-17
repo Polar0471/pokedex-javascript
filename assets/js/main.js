40964dc83d58fb514b8a7db1b47e6ed7f68f3ee8
Polar0471 <rodrigoarth512@gmail.com>
2024-05-17 18:50:30 -0300

    manipulando multiplas requisicões em paralelo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,30 +1,19 @@
 
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
-
-// fetch retorna uma promisse (promessa de um resultado, promessa de uma resposta) 
-// uma promisse nos ajuda com o processamento assíncrono
-// no processamento assíncrono, temos todo o processo de enviar uma requisicão para o servidor e ele dar uma resposta. A grande questão é que essa resposta é dada de acordo com o tempo de processamento do servidor
-// processamento assíncrono -> processamento que vai ser executado e vc não tem uma resposta de imediato
-
-// then é utilizado para tratar o caso de sucesso do fetch. 'Se der certo, então...'
-// catch é utilizado para tratar o caso de fracasso
-// um finally trata as coisas independentemente do sucesso e do fracasso
-
-// vc pode encadear os resultados dos thens
+function convertPokemonTypesToLi(pokemonTypes) {
+    return pokemonTypes.map((typeSlot) => `<li class="type">${typeSlot.type.name}</li>`);
+}
 
 function convertPokemonToLi(pokemon) {
     return `
         <li class="pokemon">
-            <span class="number">#001</span>
+            <span class="number">#${pokemon.order}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
                 <ol class="types">
-                    <li class="type">grass</li>
-                    <li class="type">poison</li>
+                    ${convertPokemonToLi(pokemon.types).join('')}
                 </ol>
+                
                 <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
             </div>
         </li>
@@ -34,8 +23,9 @@ function convertPokemonToLi(pokemon) {
 // appendChild concatena um filho
 const pokemonList = document.getElementById('pokemonList');
 
-pokeApi.getPokemons().then((pokemons = []) => {    
-    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('');
+pokeApi.getPokemons().then((pokemons = []) => {   
+    const newHtml = pokemons.map(convertPokemonToLi).join(''); 
+    pokemonList.innerHTML += newHtml;
 });
 
 
--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -3,10 +3,25 @@
 
 const pokeApi = {}
 
+pokeApi.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url)
+        .then((response) => response.json());
+}
+
 pokeApi.getPokemons = (offset = 0, limit = 10) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+    
     return fetch(url)
         .then((response) => response.json())  
         .then((jsonBody) => jsonBody.results)
-        .catch((error) => console.error(error))
+        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+        .then((detailsRequests) => Promise.all(detailsRequests))
+        .then((pokemonDetails) => pokemonDetails);
 }
+
+// buscamos a lista de pokemons
+// obtemos o response e convertemos para json
+// os jsons vem cheios de detalhes, então especificamos que queriamos somente os resultados
+// após isso mapeamos a lista de pokemons em uma lista de requisicões dos detalhes dos pokemons
+// assim fazemos um novo fetch para a url do pokemon que eu estou querendo acessar e convertendo a response que ele me der para um json
+// dessa forma, temos a lista de requisicões e estamos esperando que todas as requisicões terminem 
